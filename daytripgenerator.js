f8a1121b2ec7f90064e8633cd05d69353d7f637d
Lee X <lee52597@gmail.com>
2021-03-05 12:28:10 -0500

    Created a random number generator function for decided which results are chosen. Created arrays for each choice given to the user. Outlined most functions that will be used.

---
+++ daytripgenerator.js
@@ -0,0 +1,15 @@
+function getRandomInt(max) { // chooses random number
+    return Math.floor(Math.random() * Math.floor(max));
+}
+//function rebuild array
+//function make random choice
+//function ask if choices are acceptable
+//function print to console.
+//function ask which are unacceptable
+//function
+
+
+let destinationArray = ['Beach','Woods','Movie theater','Park'];
+let restaurantArray = ['McDonalds','Wendys','Arbys','Chipotle'];
+let transportationArray = ['Car','Bus','Rental','Uber'];
+let entertainmentArray = ['Play cards','Star watching','Painting','Eat snacks'];
