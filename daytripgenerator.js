2d97e964636c5d9356e665bc6f9730dfc22d9805
Lee X <lee52597@gmail.com>
2021-03-05 12:50:03 -0500

    Crafting an initial roll sheet. To be changed. rollArray Saves inital results.

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -1,6 +1,14 @@
 function getRandomInt(max) { // chooses random number
     return Math.floor(Math.random() * Math.floor(max));
 }
+function roll(array){
+    return array[getRandomInt(array.length)];
+}
+function assignRoll(idArray, number){
+    switch(number){
+        case 0:
+    }
+}
 //function rebuild array
 //function make random choice
 //function ask if choices are acceptable
@@ -13,3 +21,21 @@ let destinationArray = ['Beach','Woods','Movie theater','Park'];
 let restaurantArray = ['McDonalds','Wendys','Arbys','Chipotle'];
 let transportationArray = ['Car','Bus','Rental','Uber'];
 let entertainmentArray = ['Play cards','Star watching','Painting','Eat snacks'];
+let rollArray = [];
+rollArray.length = 4;
+for(let i = 0; i < 4; i++){
+    switch(i){
+        case 0:
+            rollArray[i] = roll(destinationArray);
+            break;
+        case 1:
+            rollArray[i] = roll(restaurantArray);
+            break;
+        case 2:
+            rollArray[i] = roll(transportationArray);
+            break;
+        case 3:
+            rollArray[i] = roll(entertainmentArray);
+            break;
+    }
+}
